1ce5478e5e9092a88e55a39cc6111a4303fa5099
NettoVieira <joao.fvneto@hotmail.com>
2020-09-11 06:56:45 -0300

    Adicionando Croohair e colocando e adicionando evento de mousedown

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const photoFile = document.getElementById('photo-file');
+let image = document.getElementById('photo-preview');   
+
+//select && preview image
 
 document.getElementById('select-image').onclick = function(){
   photoFile.click();  
@@ -13,10 +16,22 @@ window.addEventListener('DOMContentLoaded', () => {
     reader.readAsDataURL(file);
 
     reader.onload = function(event) {
-      let image = document.getElementById('photo-preview');   
       image.src = event.target.result;
     };
   });
 });
 
-//Parei no 15:59 do video, continuar amanhã cedo!
+//Selection tool
+
+const events = {
+  mouseover(){
+    this.style.cursor = 'crosshair'
+  },
+  mousedown(){},
+  mousemove(){},
+  mouseup(){}
+};
+
+Object.keys(events).forEach(eventName => {
+  image.addEventListener(eventName, events[eventName]);
+});
